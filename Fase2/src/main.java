e63129e4a5405821f4e642d2ef7504d694a366a9
Kevin García <gar13177@uvg.edu.gt>
2015-09-28 23:44:47 -0600

    Lab 6 Terminado
    Falta revisar el '-' en la produccion de string

--- Fase2/src/main.java
+++ Fase2/src/main.java
@@ -20,13 +20,29 @@ public class main {
         // TODO code application logic here
         Reader reader;
         Parser parser;
+        CodeGenerator code;
         try{
             reader = new Reader("prueba.txt");
             parser = new Parser(reader);
             String str = "Archivo de evaluacion: prueba.txt\n";
             str += "Para identificar errores revisar archivo: log.txt";
-            System.out.println("Valido: "+parser.Cocol());
+            boolean succ = parser.Cocol();
+            System.out.println("Valido: "+succ);
             System.out.println(str);
+            
+            if (succ){// lectura con exito
+                System.out.println("Exito");
+                reader = new Reader("entrada.txt");//archivo de entrada
+                code = new CodeGenerator(reader,parser.getTokens());
+                System.out.println("Valido: "+code.TryCode());
+                
+                /**
+                 * por el momento solo tengo conjuntos
+                 * el parser tendria que devolver un listado de conjuntos
+                 * luego la lectura solo es recorrer caracter por caracter
+                 * revisar a que conjunto pertenece cada uno
+                 */
+            }
             
         } catch (IOException e){
             System.out.println(e.toString());
